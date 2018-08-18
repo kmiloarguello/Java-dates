b61861425e8de57d07d31fc052b1b6bc1bc5a695
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2018-08-18 14:43:02 -0500

    User type date and sum # of days and program shows the final date

--- src/xyz/camiloarguello/fecha/Main.java
+++ src/xyz/camiloarguello/fecha/Main.java
@@ -1,13 +1,21 @@
 package xyz.camiloarguello.fecha;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Fecha fecha = new Fecha("1/4/1993");
-		fecha.addDias(1);
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Ingrese una fecha (dd/mm/aaaa) ");
+		String sFecha = sc.next();
 		
-		System.out.println(fecha);
-
+		Fecha fecha = new Fecha(sFecha);
+		System.out.println("La fecha ingresada es: " + fecha);
+		
+		System.out.print("Ingrese los días a sumar: ");
+		int diasSum = sc.nextInt();
+		
+		fecha.addDias(diasSum);
+		System.out.print("Sumando " + diasSum + " días, la nueva fecha es " + fecha);
 	}
 
 }
